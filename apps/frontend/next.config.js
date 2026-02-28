277dfc5178d029aaa41171e85b3e2c9f283d85f2
Rakesh Bhagat <rakeshbhagat333@gmail.com>
2026-02-28 23:05:52 +0530

    some changes

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   transpilePackages: ['@draw-app/common'],
     outputFileTracingRoot: path.join(__dirname, '../../'),
   
